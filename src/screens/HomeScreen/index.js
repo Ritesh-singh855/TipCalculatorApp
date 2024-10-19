6ba0ffc539cb211b5b17fffdc4db0fb1022f9dae
Ritesh singh <kashyapritesh85@gmail.com>
2024-10-19 11:46:03 +0530

    enhaced code

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,27 +1,40 @@
-import React , { useState }  from 'react';
-import { View, Text, TextInput, Button, TouchableOpacity  } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
-import { setBillAmount, setTipPercentage, setNumberOfPeople } from '../../redux/action';
-import { styles } from './style';
+import React, {useState} from 'react';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
+import {
+  setBillAmount,
+  setTipPercentage,
+  setNumberOfPeople,
+} from '../../redux/action';
+import {styles} from './style';
 
 const TipCalculator = () => {
   const dispatch = useDispatch();
-  const { billAmount, tipPercentage, numberOfPeople } = useSelector(state => state.tipCalculator);
+  const {billAmount, tipPercentage, numberOfPeople} = useSelector(
+    state => state.tipCalculator,
+  );
   const [isNumberOfPeopleValid, setIsNumberOfPeopleValid] = useState(true);
+  const [isBillAmountValid, setIsBillAmountValid] = useState(true);
+  const [isShowNumberOfPeople, setIsShowNumberOfPeople] = useState(true);
 
-  const handleSetBillAmount = (amount) => {
-    dispatch(setBillAmount(amount));
+  const handleSetBillAmount = amount => {
+    if (amount === '' || parseFloat(amount) > 0) {
+      setIsBillAmountValid(true);
+      dispatch(setBillAmount(amount));
+    } else {
+      setIsBillAmountValid(false);
+    }
   };
 
-  const handleSetTipPercentage = (percentage) => {
+  const handleSetTipPercentage = percentage => {
     dispatch(setTipPercentage(percentage));
   };
 
-  const handleSetNumberOfPeople = (number) => {
+  const handleSetNumberOfPeople = number => {
     const parsedNumber = parseInt(number);
     if (isNaN(parsedNumber) || parsedNumber < 1) {
       setIsNumberOfPeopleValid(false);
-      dispatch(setNumberOfPeople(1)); // Default to 1 if invalid
+      dispatch(setNumberOfPeople(1));
     } else {
       setIsNumberOfPeopleValid(true);
       dispatch(setNumberOfPeople(parsedNumber));
@@ -55,34 +68,49 @@ const TipCalculator = () => {
         value={numberOfPeople}
         onChangeText={handleSetBillAmount}
       />
-       <View style={styles.tipButtons}>
-        {[5, 10, 15, 25, 50].map((percentage) => (
+      {!isBillAmountValid && (
+        <Text style={styles.errorText}>Please enter valid billing amount.</Text>
+      )}
+      <View style={styles.tipButtons}>
+        {[5, 10, 15, 25, 50].map(percentage => (
           <TouchableOpacity
             key={percentage}
             style={[
               styles.tipButton,
               tipPercentage === percentage && styles.activeButton,
             ]}
-            onPress={() => handleSetTipPercentage(percentage)}
-          >
+            onPress={() => handleSetTipPercentage(percentage)}>
             <Text style={styles.buttonText}>{`${percentage}%`}</Text>
           </TouchableOpacity>
         ))}
       </View>
-      <TextInput
-        style={styles.input}
-        placeholder="Number of people"
-        keyboardType="numeric"
-        value={numberOfPeople}
-        onChangeText={handleSetNumberOfPeople}
-      />
-        {!isNumberOfPeopleValid && (
-        <Text style={styles.errorText}>Please enter at least one person.</Text>
+      {isBillAmountValid  && (
+        <>
+          <TextInput
+            style={styles.input}
+            placeholder="Number of people"
+            keyboardType="numeric"
+            value={numberOfPeople}
+            onChangeText={handleSetNumberOfPeople}
+          />
+          {!isNumberOfPeopleValid && (
+            <Text style={styles.errorText}>
+              Please enter at least one person.
+            </Text>
+          )}
+        </>
       )}
+
       <View style={styles.resultContainer}>
-        <Text style={styles.resultText}>Tip Amount: ₹ {calculateTip().toFixed(2)}</Text>
-        <Text style={styles.resultText}>Total Amount: ₹ {calculateTotal().toFixed(2)}</Text>
-        <Text style={styles.resultText}>Amount Per Person: ₹ {calculatePerPerson().toFixed(2)}</Text>
+        <Text style={styles.resultText}>
+          Tip Amount: ₹ {calculateTip().toFixed(2)}
+        </Text>
+        <Text style={styles.resultText}>
+          Total Amount: ₹ {calculateTotal().toFixed(2)}
+        </Text>
+        <Text style={styles.resultText}>
+          Amount Per Person: ₹ {calculatePerPerson().toFixed(2)}
+        </Text>
       </View>
     </View>
   );
